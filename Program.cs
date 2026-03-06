b3a17a031814a3e3ebf9e6d34f1c4b3a5efc2db2
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 19:26:00 -0600

    Logica Aplicada!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿// Cree un programa que calcule la suma de los números del 1 al 10 utilizando do-while.
+
+
+int suma = 0;
+int num = 1;
+do
+{
+    suma = suma + num;
+    num++;
+
+}while (num <=10);
+Console.WriteLine(" La suma es = "+ suma);
